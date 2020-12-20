5ef9142452d6cbc870e332bb6e4e8780640886f3
otoyo <hiroki.t.1988@gmail.com>
2020-12-20 20:46:48 +0900

    Change date locale

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -3,7 +3,7 @@ export const getBlogLink = (slug: string) => {
 }
 
 export const getDateStr = date => {
-  return new Date(date).toLocaleString('en-US', {
+  return new Date(date).toLocaleString('ja-JP', {
     month: 'long',
     day: '2-digit',
     year: 'numeric',
