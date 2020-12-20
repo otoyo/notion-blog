821139fd0b756d416fcb391fbe45857c48572b4b
otoyo <hiroki.t.1988@gmail.com>
2020-12-20 21:20:28 +0900

    Change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,17 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
-        <img
-          src="https://vercel.com/button"
-          height={46}
-          width={132}
-          alt="deploy to Vercel button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
+      <div>
+        <span>Powered by </span>
         <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
+          Notion Blog
         </ExtLink>
-      </span>
+      </div>
+      <div>
+        <span>Copyright © 2015-2020 </span>
+        <ExtLink href="https://twitter.com/otoyo0122">@otoyo0122</ExtLink>
+        <span>. All Rights Reserved.</span>
+      </div>
     </footer>
   </>
 )
