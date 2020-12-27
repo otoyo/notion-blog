daa842205a98ff5e612b30caf581ccd9d66942ed
otoyo <hiroki.t.1988@gmail.com>
2020-12-27 22:59:29 +0900

    Add Google Analytics tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      if (location.host === 'alpacat.com') {
+        gtag.pageview(url)
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
