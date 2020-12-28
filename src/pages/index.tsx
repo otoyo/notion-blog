6c742674a11d06cafcf42f2389eea049d87cf27c
otoyo <hiroki.t.1988@gmail.com>
2020-12-28 18:24:57 +0900

    Fix home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,61 +4,33 @@ import ExtLink from '../components/ext-link'
 import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
+import { getBlogLink } from '../lib/blog-helpers'
 
 export default () => (
   <>
     <Header titlePre="" />
     <div className={sharedStyles.layout}>
-      <img
-        src="/vercel-and-notion.png"
-        height="85"
-        width="250"
-        alt="Vercel + Notion"
-      />
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
-
-      <Features />
+      <img src="/young_alpaca.png" height="473" width="200" alt="Alpacat" />
 
       <div className="explanation">
         <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
+          プログラミングやマネジメント、読んだ本のまとめを中心に書いているエンジニアブログ
         </p>
+      </div>
 
+      <h2>書いている人</h2>
+
+      <div className="explanation">
+        <ExtLink href="https://twitter.com/otoyo0122">@otoyo0122</ExtLink>
         <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
+          CREというWebエンジニアでマネージャーな人です。エディタはNeoVimがお気に入り。記事内容に誤りがある際はお知らせください。CREでは一緒に働く仲間を募集しています。
+          <Link
+            href="/blog/recruitment-cre"
+            as={getBlogLink('recruitment-cre')}
+            passHref
+          >
+            <a>→詳しく</a>
+          </Link>
         </p>
       </div>
     </div>
