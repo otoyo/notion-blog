9eba1e340f571372453602d4f47661af847a0970
otoyo <hiroki.t.1988@gmail.com>
2020-12-30 16:52:01 +0900

    Change html lang to ja

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from '../lib/gtag'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="ja">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
