17a7b75b20be477fa0fff6b3c1933d4143efcba5
otoyo <hiroki.t.1988@gmail.com>
2021-01-05 13:55:43 +0900

    WIP

---
+++ src/pages/api/atom.ts
@@ -0,0 +1,95 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+import { textBlock } from './notion/renderers'
+import getBlogIndex from '../../lib/notion/getBlogIndex'
+import getNotionUsers from './notion/getNotionUsers'
+import { postIsPublished, getBlogLink } from './blog-helpers'
+
+const NOW = new Date().toJSON()
+
+function mapToAuthor(author) {
+  return `<author><name>${author.full_name}</name></author>`
+}
+
+function decode(string) {
+  return string
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+function mapToEntry(post) {
+  return `
+    <entry>
+      <id>https://alpacat.com${post.link}</id>
+      <title>${decode(post.title)}</title>
+      <link href="https://alpacat.com${post.link}"/>
+      <updated>${new Date(post.date).toJSON()}</updated>
+      <content type="xhtml">
+        <div xmlns="http://www.w3.org/1999/xhtml">
+          ${renderToStaticMarkup(post.Excerpt)}
+        </div>
+      </content>
+      ${(post.authors || []).map(mapToAuthor).join('\n      ')}
+    </entry>`
+}
+
+function concat(total, item) {
+  return total + item
+}
+
+function createRSS(blogPosts = []) {
+  const postsString = blogPosts.map(mapToEntry).reduce(concat, '')
+
+  return `<?xml version="1.0" encoding="utf-8"?>
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>アルパカログ</title>
+    <subtitle>アルパカログの更新情報</subtitle>
+    <link href="https://alpacat.com/atom" rel="self" type="application/rss+xml"/>
+    <link href="https://alpacat.com" />
+    <updated>${NOW}</updated>
+    <id>alpacat.com/atom</id>${postsString}
+  </feed>`
+}
+
+export default async function(req: IncomingMessage, res: ServerResponse) {
+  res.setHeader('Content-Type', 'text/xml')
+  try {
+    const postsTable = await getBlogIndex()
+    const neededAuthors = new Set<string>()
+
+    const posts = Object.keys(postsTable)
+      .map(slug => {
+        const post = postsTable[slug]
+        if (!postIsPublished(post)) return
+
+        post.authors = post.Authors || []
+
+        for (const author of post.authors) {
+          neededAuthors.add(author)
+        }
+        return post
+      })
+      .filter(Boolean)
+      .sort((a, b) => (b.Date || 0) - (a.Date || 0))
+
+    const { users } = await getNotionUsers([...neededAuthors])
+
+    posts.forEach(post => {
+      post.authors = post.authors.map(id => users[id])
+      post.link = getBlogLink(post.Slug)
+      post.title = post.Page
+      post.date = post.Date
+    })
+
+    res.write(atom)
+    res.end()
+  } catch (e) {
+    console.log(e)
+    res.statusCode = 500
+    res.end()
+  }
+}
