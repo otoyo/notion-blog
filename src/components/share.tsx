9bd4d7236d247b5bde87daa5d13e066f295f12f4
otoyo <hiroki.t.1988@gmail.com>
2021-03-21 12:43:42 +0900

    Display social share buttons

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -1,11 +1,34 @@
 // components/Share.tsx
 import React from 'react'
-import { TwitterIcon, TwitterShareButton } from 'react-share'
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  HatenaIcon,
+  HatenaShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from 'react-share'
 
 export default ({ text = '', url = '' }) => {
   return (
-    <TwitterShareButton url={url} title={text}>
-      <TwitterIcon size={32} round={true} />
-    </TwitterShareButton>
+    <>
+      <ul className="shareButtons">
+        <li>
+          <TwitterShareButton url={url} title={text}>
+            <TwitterIcon size={32} round={true} />
+          </TwitterShareButton>
+        </li>
+        <li>
+          <FacebookShareButton url={url}>
+            <FacebookIcon size={32} round={true} />
+          </FacebookShareButton>
+        </li>
+        <li>
+          <HatenaShareButton url={url} title={text}>
+            <HatenaIcon size={32} round={true} />
+          </HatenaShareButton>
+        </li>
+      </ul>
+    </>
   )
 }
