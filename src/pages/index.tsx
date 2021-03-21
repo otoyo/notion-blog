f7757e5e77b9d1a66ff2724bb3270411e8c1a331
otoyo <hiroki.t.1988@gmail.com>
2021-03-21 12:58:41 +0900

    Remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 import { getBlogLink } from '../lib/blog-helpers'
 
