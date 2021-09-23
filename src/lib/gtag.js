88ecf953234897dec9d69e643c5c6a302cbbfe22
otoyo <hiroki.t.1988@gmail.com>
2021-09-23 11:54:48 +0900

    Allow null in gtag.event

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -8,7 +8,7 @@ export const pageview = url => {
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
+export const event = ({ action, category, label, value = null }) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
