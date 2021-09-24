1d2d64626af57dfd0f992e3ea67ec95218667dfe
otoyo <hiroki.t.1988@gmail.com>
2021-09-24 17:26:19 +0900

    Use GoogleAnalytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import { GA_TRACKING_ID } from '../lib/gtag'
+import GoogleAnalytics from '../components/google-analytics'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_TRACKING_ID}',{page_path:window.location.pathname});`,
-            }}
-          />
+          <GoogleAnalytics />
         </Head>
         <body>
           <Main />
