07044968d5a8ea80c554c44cdcf401eeb1856c0c
otoyo <hiroki.t.1988@gmail.com>
2021-09-24 14:53:02 +0900

    Add GA_TRACKING_ID to constants

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -3,12 +3,14 @@ const path = require('path')
 
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
   NOTION_API_SECRET,
   DATABASE_ID,
+  GA_TRACKING_ID,
   BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
