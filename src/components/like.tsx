7faab6576ba74a12f7c13af92d35ef031acd4716
otoyo <hiroki.t.1988@gmail.com>
2021-10-03 16:51:01 +0900

    Add Like component

---
+++ src/components/like.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import * as gtag from '../lib/gtag'
+import sharedStyles from '../styles/shared.module.css'
+import Heart from './svgs/heart'
+
+class Like extends React.Component<any, any> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: 0,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    gtag.like({
+      contentType: 'article',
+      itemId: this.props.id,
+    })
+    this.setState({ active: 1 })
+  }
+
+  render() {
+    return (
+      <>
+        <button className={sharedStyles.likeButton} onClick={this.handleClick}>
+          <Heart width={32} height={32} active={this.state.active} />
+        </button>
+      </>
+    )
+  }
+}
+
+export default Like
