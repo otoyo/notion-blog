ef4b3afbb5213c1d4d8936a2523c0feebdad4552
otoyo <hiroki.t.1988@gmail.com>
2021-11-03 22:28:35 +0900

    Consider GA_TRACKING_ID not set

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,6 +2,10 @@ export const GA_TRACKING_ID = 'G-M08P8YF8KX'
 
 // https://developers.google.com/analytics/devguides/migration/measurement/virtual-pageviews
 export const pageview = (title, url) => {
+  if (!GA_TRACKING_ID) {
+    return
+  }
+
   window.gtag('config', GA_TRACKING_ID, {
     page_title: title,
     page_location: url,
@@ -10,6 +14,10 @@ export const pageview = (title, url) => {
 
 // https://developers.google.com/gtagjs/reference/ga4-events
 export const share = ({ method, contentType, itemId = null }) => {
+  if (!GA_TRACKING_ID) {
+    return
+  }
+
   window.gtag('event', 'share', {
     method: method,
     content_type: contentType,
@@ -18,6 +26,10 @@ export const share = ({ method, contentType, itemId = null }) => {
 }
 
 export const like = ({ contentType, itemId }) => {
+  if (!GA_TRACKING_ID) {
+    return
+  }
+
   window.gtag('event', 'like', {
     content_type: contentType,
     item_id: itemId,
