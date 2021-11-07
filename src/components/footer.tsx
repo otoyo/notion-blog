6da500707f1c7c797586be1204c3b63043d64747
otoyo <hiroki.t.1988@gmail.com>
2021-11-07 11:06:06 +0900

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => (
     <footer>
       <div>
         <span>Powered by </span>
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          Notion Blog
+        <ExtLink href="https://github.com/otoyo/easy-notion-blog">
+          easy-notion-blog
         </ExtLink>
       </div>
       <div>
