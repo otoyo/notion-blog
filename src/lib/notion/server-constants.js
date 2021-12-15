c4152a7740ccac0c7462e1b1be7e5e26735211e8
otoyo <hiroki.t.1988@gmail.com>
2021-12-15 21:44:30 +0900

    Add NEXT_PUBLIC_GA_TRACKING_ID to env

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -4,7 +4,7 @@ const path = require('path')
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
-const GA_TRACKING_ID = process.env.GA_TRACKING_ID
+const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
@@ -12,7 +12,7 @@ module.exports = {
   NOTION_API_SECRET,
   DATABASE_ID,
   NEXT_PUBLIC_URL,
-  GA_TRACKING_ID,
+  NEXT_PUBLIC_GA_TRACKING_ID,
   BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
