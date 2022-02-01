0195d7a59182c64b32a769a34a3befc3d6f79fb3
otoyo <hiroki.t.1988@gmail.com>
2022-02-01 22:40:01 +0900

    Refine components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,87 +1,36 @@
 import Link from 'next/link'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-import ExtLink from './ext-link'
 import styles from '../styles/header.module.css'
 
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'ホーム', page: '/' },
-  { label: 'ブログ', page: '/blog' },
-  { label: '読者になる', page: '/subscribe' },
-]
+interface NavItem {
+  label: string
+  path: string
+}
 
-const defaultUrl = 'https://alpacat.com'
-const defaultTitle = 'アルパカログ'
-const defaultOgImageUrl = 'https://alpacat.com/og-image.jpeg'
-const defaultDescription =
-  'Notion Blogのカスタマイズ、マネジメント、プログラミングや読んだ本のまとめなどが中心のブログ'
-
-const Header = ({
-  path = '',
-  titlePre = '',
-  description = '',
-  ogImageUrl = '',
-}) => {
+const Header = () => {
   const { pathname } = useRouter()
+
+  const navItems: NavItem[] = [
+    { label: 'ホーム', path: '/' },
+    { label: 'ブログ', path: '/blog' },
+    { label: '読者になる', path: '/subscribe' },
+  ]
 
   return (
     <header className={styles.header}>
-      <Head>
-        <title>
-          {!titlePre ? defaultTitle : `${titlePre} - ${defaultTitle}`}
-        </title>
-        <meta
-          name="description"
-          content={!description ? defaultDescription : description}
-        />
-        <meta property="og:url" content={`${defaultUrl}${path}`} />
-        <meta
-          property="og:title"
-          content={!titlePre ? defaultTitle : titlePre}
-        />
-        <meta
-          property="og:description"
-          content={!description ? defaultDescription : description}
-        />
-        <meta
-          property="og:image"
-          content={!ogImageUrl ? defaultOgImageUrl : ogImageUrl}
-        />
-        <meta name="twitter:site" content="@otoyo0122" />
-        <meta
-          name="twitter:card"
-          content={!ogImageUrl ? 'summary' : 'summary_large_image'}
-        />
-        <meta
-          name="twitter:image"
-          content={!ogImageUrl ? defaultOgImageUrl : ogImageUrl}
-        />
-        <link rel="canonical" href={`${defaultUrl}${path}`} />
-        <link
-          rel="alternate"
-          type="application/atom+xml"
-          href="/atom"
-          title="アルパカログのフィード"
-        />
-      </Head>
       <h1>
         <Link href="/" passHref>
           <a>アルパカログ</a>
         </Link>
       </h1>
+
       <ul>
-        {navItems.map(({ label, page, link }) => (
+        {navItems.map(({ label, path }) => (
           <li key={label}>
-            {page ? (
-              <Link href={page} passHref>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <ExtLink href={link}>{label}</ExtLink>
-            )}
+            <Link href={path} passHref>
+              <a className={pathname === path ? 'active' : null}>{label}</a>
+            </Link>
           </li>
         ))}
       </ul>
