1a3da673f3d18e094ca565a8a2debbe5e958f640
otoyo <hiroki.t.1988@gmail.com>
2022-12-18 11:00:22 +0900

    Define SubscribeHead

---
+++ app/subscribe/head.tsx
@@ -0,0 +1,7 @@
+import DocumentHead from '../../components/document-head'
+
+const SubscribeHead = async () => (
+  <DocumentHead title="購読する" description="アルパカログを購読する" path="/subscribe" />
+)
+
+export default SubscribeHead
