a31208a744debdec36ed247f096a9e0b65c94aac
otoyo <hiroki.t.1988@gmail.com>
2023-02-03 22:40:37 +0900

    Delete Next.js files

--- app/subscribe/head.tsx
+++
@@ -1,7 +0,0 @@
-import DocumentHead from '../../components/document-head'
-
-const SubscribeHead = async () => (
-  <DocumentHead title="購読する" description="アルパカログを購読する" path="/subscribe" />
-)
-
-export default SubscribeHead
