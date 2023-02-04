6a3e37bc2de37e0a2632060a53d454eb632b2857
otoyo <hiroki.t.1988@gmail.com>
2023-02-04 16:55:26 +0900

    Add and edit constants

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -1,8 +1,10 @@
 export const NOTION_API_SECRET = import.meta.env.NOTION_API_SECRET
 export const DATABASE_ID = import.meta.env.DATABASE_ID
+export const INDEX_PAGE_ID = import.meta.env.INDEX_PAGE_ID
+export const SUBSCRIPTION_PAGE_ID = import.meta.env.SUBSCRIPTION_PAGE_ID
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID
 export const NUMBER_OF_POSTS_PER_PAGE = 10
 export const REQUEST_TIMEOUT_MS = parseInt(import.meta.env.REQUEST_TIMEOUT_MS || '10000', 10)
 
-export const PUBLIC_SITE_TITLE = 'astro-notion-blog'
-export const PUBLIC_SITE_DESCRIPTION = 'astro-notion-blog is generated statically by Astro'
+export const PUBLIC_SITE_TITLE = 'アルパカログ'
+export const PUBLIC_SITE_DESCRIPTION = 'Notion Blogのカスタマイズ、マネジメント、プログラミングや読んだ本のまとめなどが中心のブログ'
