e5d62129056fb58b94e387050412eff6d40f631d
otoyo <hiroki.t.1988@gmail.com>
2023-02-14 13:56:35 +0900

    Format by Prettier

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -48,7 +48,6 @@ const client = new Client({
 
 let cache: Post[] | null = null
 
-
 export async function getAllPosts(): Promise<Post[]> {
   if (cache !== null) {
     return Promise.resolve(cache)
@@ -97,8 +96,8 @@ export async function getAllPosts(): Promise<Post[]> {
   }
 
   cache = results
-    .filter(pageObject => _validPageObject(pageObject))
-    .map(pageObject => _buildPost(pageObject))
+    .filter((pageObject) => _validPageObject(pageObject))
+    .map((pageObject) => _buildPost(pageObject))
   return cache
 }
 
@@ -110,7 +109,7 @@ export async function getPosts(pageSize = 10): Promise<Post[]> {
 export async function getRankedPosts(pageSize = 10): Promise<Post[]> {
   const allPosts = await getAllPosts()
   return allPosts
-    .filter(post => !!post.Rank)
+    .filter((post) => !!post.Rank)
     .sort((a, b) => {
       if (a.Rank > b.Rank) {
         return -1
@@ -122,16 +121,21 @@ export async function getRankedPosts(pageSize = 10): Promise<Post[]> {
     .slice(0, pageSize)
 }
 
-export async function getPostBySlug(slug: string): Promise<Post|null> {
+export async function getPostBySlug(slug: string): Promise<Post | null> {
   const allPosts = await getAllPosts()
-  return allPosts.find(post => post.Slug === slug) || null
+  return allPosts.find((post) => post.Slug === slug) || null
 }
 
-export async function getPostsByTag(tagName: string, pageSize = 10): Promise<Post[]> {
+export async function getPostsByTag(
+  tagName: string,
+  pageSize = 10
+): Promise<Post[]> {
   if (!tagName) return []
 
   const allPosts = await getAllPosts()
-  return allPosts.filter(post => post.Tags.find((tag) => tag.name === tagName)).slice(0, pageSize)
+  return allPosts
+    .filter((post) => post.Tags.find((tag) => tag.name === tagName))
+    .slice(0, pageSize)
 }
 
 // page starts from 1 not 0
@@ -149,13 +153,18 @@ export async function getPostsByPage(page: number): Promise<Post[]> {
 }
 
 // page starts from 1 not 0
-export async function getPostsByTagAndPage(tagName: string, page: number): Promise<Post[]> {
+export async function getPostsByTagAndPage(
+  tagName: string,
+  page: number
+): Promise<Post[]> {
   if (page < 1) {
     return []
   }
 
   const allPosts = await getAllPosts()
-  const posts = allPosts.filter(post => post.Tags.find((tag) => tag.name === tagName))
+  const posts = allPosts.filter((post) =>
+    post.Tags.find((tag) => tag.name === tagName)
+  )
 
   const startIndex = (page - 1) * NUMBER_OF_POSTS_PER_PAGE
   const endIndex = startIndex + NUMBER_OF_POSTS_PER_PAGE
@@ -165,13 +174,21 @@ export async function getPostsByTagAndPage(tagName: string, page: number): Promi
 
 export async function getNumberOfPages(): Promise<number> {
   const allPosts = await getAllPosts()
-  return Math.floor(allPosts.length / NUMBER_OF_POSTS_PER_PAGE) + (allPosts.length % NUMBER_OF_POSTS_PER_PAGE > 0 ? 1 : 0)
+  return (
+    Math.floor(allPosts.length / NUMBER_OF_POSTS_PER_PAGE) +
+    (allPosts.length % NUMBER_OF_POSTS_PER_PAGE > 0 ? 1 : 0)
+  )
 }
 
 export async function getNumberOfPagesByTag(tagName: string): Promise<number> {
   const allPosts = await getAllPosts()
-  const posts = allPosts.filter(post => post.Tags.find((tag) => tag.name === tagName))
-  return Math.floor(posts.length / NUMBER_OF_POSTS_PER_PAGE) + (posts.length % NUMBER_OF_POSTS_PER_PAGE > 0 ? 1 : 0)
+  const posts = allPosts.filter((post) =>
+    post.Tags.find((tag) => tag.name === tagName)
+  )
+  return (
+    Math.floor(posts.length / NUMBER_OF_POSTS_PER_PAGE) +
+    (posts.length % NUMBER_OF_POSTS_PER_PAGE > 0 ? 1 : 0)
+  )
 }
 
 export async function getAllBlocksByBlockId(blockId: string): Promise<Block[]> {
@@ -199,7 +216,7 @@ export async function getAllBlocksByBlockId(blockId: string): Promise<Block[]> {
     }
   }
 
-  const allBlocks = results.map(blockObject => _buildBlock(blockObject))
+  const allBlocks = results.map((blockObject) => _buildBlock(blockObject))
 
   for (let i = 0; i < allBlocks.length; i++) {
     const block = allBlocks[i]
@@ -208,9 +225,17 @@ export async function getAllBlocksByBlockId(blockId: string): Promise<Block[]> {
       block.Table.Rows = await _getTableRows(block.Id)
     } else if (block.Type === 'column_list' && block.ColumnList) {
       block.ColumnList.Columns = await _getColumns(block.Id)
-    } else if (block.Type === 'bulleted_list_item' && block.BulletedListItem && block.HasChildren) {
+    } else if (
+      block.Type === 'bulleted_list_item' &&
+      block.BulletedListItem &&
+      block.HasChildren
+    ) {
       block.BulletedListItem.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'numbered_list_item' && block.NumberedListItem && block.HasChildren) {
+    } else if (
+      block.Type === 'numbered_list_item' &&
+      block.NumberedListItem &&
+      block.HasChildren
+    ) {
       block.NumberedListItem.Children = await getAllBlocksByBlockId(block.Id)
     } else if (block.Type === 'to_do' && block.ToDo && block.HasChildren) {
       block.ToDo.Children = await getAllBlocksByBlockId(block.Id)
@@ -218,19 +243,40 @@ export async function getAllBlocksByBlockId(blockId: string): Promise<Block[]> {
       block.SyncedBlock.Children = await _getSyncedBlockChildren(block)
     } else if (block.Type === 'toggle' && block.Toggle) {
       block.Toggle.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'paragraph' && block.Paragraph && block.HasChildren) {
+    } else if (
+      block.Type === 'paragraph' &&
+      block.Paragraph &&
+      block.HasChildren
+    ) {
       block.Paragraph.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'heading_1' && block.Heading1 && block.HasChildren) {
+    } else if (
+      block.Type === 'heading_1' &&
+      block.Heading1 &&
+      block.HasChildren
+    ) {
       block.Heading1.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'heading_2' && block.Heading2 && block.HasChildren) {
+    } else if (
+      block.Type === 'heading_2' &&
+      block.Heading2 &&
+      block.HasChildren
+    ) {
       block.Heading2.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'heading_3' && block.Heading3 && block.HasChildren) {
+    } else if (
+      block.Type === 'heading_3' &&
+      block.Heading3 &&
+      block.HasChildren
+    ) {
       block.Heading3.Children = await getAllBlocksByBlockId(block.Id)
     } else if (block.Type === 'quote' && block.Quote && block.HasChildren) {
       block.Quote.Children = await getAllBlocksByBlockId(block.Id)
     } else if (block.Type === 'callout' && block.Callout && block.HasChildren) {
       block.Callout.Children = await getAllBlocksByBlockId(block.Id)
-    } else if (block.Type === 'image' && block.Image && block.Image.File && block.Image.File.ExpiryTime) {
+    } else if (
+      block.Type === 'image' &&
+      block.Image &&
+      block.Image.File &&
+      block.Image.File.ExpiryTime
+    ) {
       if (Date.parse(block.Image.File.ExpiryTime) < Date.now()) {
         block.Image = (await getBlock(block.Id)).Image
       }
@@ -255,13 +301,18 @@ export async function getAllTags(): Promise<SelectProperty[]> {
   const allPosts = await getAllPosts()
 
   const tagNames: string[] = []
-  return allPosts.flatMap(post => post.Tags).reduce((acc, tag) => {
-    if (!tagNames.includes(tag.name)) {
-      acc.push(tag)
-      tagNames.push(tag.name)
-    }
-    return acc
-  }, [] as SelectProperty[]).sort((a: SelectProperty, b: SelectProperty) => a.name.localeCompare(b.name))
+  return allPosts
+    .flatMap((post) => post.Tags)
+    .reduce((acc, tag) => {
+      if (!tagNames.includes(tag.name)) {
+        acc.push(tag)
+        tagNames.push(tag.name)
+      }
+      return acc
+    }, [] as SelectProperty[])
+    .sort((a: SelectProperty, b: SelectProperty) =>
+      a.name.localeCompare(b.name)
+    )
 }
 
 function _buildBlock(blockObject: responses.BlockObject): Block {
@@ -314,7 +365,8 @@ function _buildBlock(blockObject: responses.BlockObject): Block {
     case 'bulleted_list_item':
       if (blockObject.bulleted_list_item) {
         const bulletedListItem: BulletedListItem = {
-          RichTexts: blockObject.bulleted_list_item.rich_text.map(_buildRichText),
+          RichTexts:
+            blockObject.bulleted_list_item.rich_text.map(_buildRichText),
           Color: blockObject.bulleted_list_item.color,
         }
         block.BulletedListItem = bulletedListItem
@@ -323,7 +375,8 @@ function _buildBlock(blockObject: responses.BlockObject): Block {
     case 'numbered_list_item':
       if (blockObject.numbered_list_item) {
         const numberedListItem: NumberedListItem = {
-          RichTexts: blockObject.numbered_list_item.rich_text.map(_buildRichText),
+          RichTexts:
+            blockObject.numbered_list_item.rich_text.map(_buildRichText),
           Color: blockObject.numbered_list_item.color,
         }
         block.NumberedListItem = numberedListItem
@@ -345,7 +398,10 @@ function _buildBlock(blockObject: responses.BlockObject): Block {
           Caption: blockObject.video.caption?.map(_buildRichText) || [],
           Type: blockObject.video.type,
         }
-        if (blockObject.video.type === 'external' && blockObject.video.external) {
+        if (
+          blockObject.video.type === 'external' &&
+          blockObject.video.external
+        ) {
           video.External = { Url: blockObject.video.external.url }
         }
         block.Video = video
@@ -357,10 +413,19 @@ function _buildBlock(blockObject: responses.BlockObject): Block {
           Caption: blockObject.image.caption?.map(_buildRichText) || [],
           Type: blockObject.image.type,
         }
-        if (blockObject.image.type === 'external' && blockObject.image.external) {
+        if (
+          blockObject.image.type === 'external' &&
+          blockObject.image.external
+        ) {
           image.External = { Url: blockObject.image.external.url }
-        } else if (blockObject.image.type === 'file' && blockObject.image.file) {
-          image.File = { Url: blockObject.image.file.url, ExpiryTime: blockObject.image.file.expiry_time }
+        } else if (
+          blockObject.image.type === 'file' &&
+          blockObject.image.file
+        ) {
+          image.File = {
+            Url: blockObject.image.file.url,
+            ExpiryTime: blockObject.image.file.expiry_time,
+          }
         }
         block.Image = image
       }
@@ -407,7 +472,10 @@ function _buildBlock(blockObject: responses.BlockObject): Block {
     case 'synced_block':
       if (blockObject.synced_block) {
         let syncedFrom: SyncedFrom | null = null
-        if (blockObject.synced_block.synced_from && blockObject.synced_block.synced_from.block_id) {
+        if (
+          blockObject.synced_block.synced_from &&
+          blockObject.synced_block.synced_from.block_id
+        ) {
           syncedFrom = {
             BlockId: blockObject.synced_block.synced_from.block_id,
           }
@@ -508,16 +576,16 @@ async function _getTableRows(blockId: string): Promise<TableRow[]> {
     }
   }
 
-  return results.map(blockObject => {
+  return results.map((blockObject) => {
     const tableRow: TableRow = {
       Id: blockObject.id,
       Type: blockObject.type,
       HasChildren: blockObject.has_children,
-      Cells: []
+      Cells: [],
     }
 
     if (blockObject.type === 'table_row' && blockObject.table_row) {
-      const cells: TableCell[] = blockObject.table_row.cells.map(cell => {
+      const cells: TableCell[] = blockObject.table_row.cells.map((cell) => {
         const tableCell: TableCell = {
           RichTexts: cell.map(_buildRichText),
         }
@@ -557,23 +625,29 @@ async function _getColumns(blockId: string): Promise<Column[]> {
     }
   }
 
-  return await Promise.all(results.map(async blockObject => {
-    const children = await getAllBlocksByBlockId(blockObject.id)
+  return await Promise.all(
+    results.map(async (blockObject) => {
+      const children = await getAllBlocksByBlockId(blockObject.id)
 
-    const column: Column = {
-      Id: blockObject.id,
-      Type: blockObject.type,
-      HasChildren: blockObject.has_children,
-      Children: children,
-    }
+      const column: Column = {
+        Id: blockObject.id,
+        Type: blockObject.type,
+        HasChildren: blockObject.has_children,
+        Children: children,
+      }
 
-    return column
-  }))
+      return column
+    })
+  )
 }
 
 async function _getSyncedBlockChildren(block: Block): Promise<Block[]> {
   let originalBlock: Block = block
-  if (block.SyncedBlock && block.SyncedBlock.SyncedFrom && block.SyncedBlock.SyncedFrom.BlockId) {
+  if (
+    block.SyncedBlock &&
+    block.SyncedBlock.SyncedFrom &&
+    block.SyncedBlock.SyncedFrom.BlockId
+  ) {
     try {
       originalBlock = await getBlock(block.SyncedBlock.SyncedFrom.BlockId)
     } catch (err) {
@@ -589,8 +663,10 @@ async function _getSyncedBlockChildren(block: Block): Promise<Block[]> {
 function _validPageObject(pageObject: responses.PageObject): boolean {
   const prop = pageObject.properties
   return (
-    !!prop.Page.title && prop.Page.title.length > 0 &&
-    !!prop.Slug.rich_text && prop.Slug.rich_text.length > 0 &&
+    !!prop.Page.title &&
+    prop.Page.title.length > 0 &&
+    !!prop.Slug.rich_text &&
+    prop.Slug.rich_text.length > 0 &&
     !!prop.Date.date
   )
 }
@@ -606,10 +682,12 @@ function _buildPost(pageObject: responses.PageObject): Post {
     Tags: prop.Tags.multi_select ? prop.Tags.multi_select : [],
     Excerpt:
       prop.Excerpt.rich_text && prop.Excerpt.rich_text.length > 0
-        ? prop.Excerpt.rich_text.map(t => t.plain_text).join('')
+        ? prop.Excerpt.rich_text.map((t) => t.plain_text).join('')
         : '',
     FeaturedImage:
-      prop.FeaturedImage.files && prop.FeaturedImage.files.length > 0 && prop.FeaturedImage.files[0].file
+      prop.FeaturedImage.files &&
+      prop.FeaturedImage.files.length > 0 &&
+      prop.FeaturedImage.files[0].file
         ? prop.FeaturedImage.files[0].file.url
         : null,
     Rank: prop.Rank.number ? prop.Rank.number : 0,
