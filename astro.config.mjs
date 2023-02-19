a49b3a4566c17e6228081c2dca4656f4e64e5169
otoyo <hiroki.t.1988@gmail.com>
2023-02-19 22:36:48 +0900

    Fix type

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const getSite = function () {
   }
 
   if (CUSTOM_DOMAIN) {
-    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`);
+    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
 
   return new URL(
