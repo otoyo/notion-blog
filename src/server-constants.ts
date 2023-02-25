a571941e1c859786df731f77974eb7da9c8e3c5b
otoyo <hiroki.t.1988@gmail.com>
2023-02-25 14:27:37 +0900

    Read from import.meta.env first

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -1,10 +1,10 @@
-export const NOTION_API_SECRET = process.env.NOTION_API_SECRET || ''
-export const DATABASE_ID = process.env.DATABASE_ID || ''
-export const INDEX_PAGE_ID = process.env.INDEX_PAGE_ID || ''
-export const SUBSCRIPTION_PAGE_ID = process.env.SUBSCRIPTION_PAGE_ID || ''
+export const NOTION_API_SECRET = import.meta.env.NOTION_API_SECRET || process.env.NOTION_API_SECRET || ''
+export const DATABASE_ID = import.meta.env.DATABASE_ID || process.env.DATABASE_ID || ''
+export const INDEX_PAGE_ID = import.meta.env.INDEX_PAGE_ID || process.env.INDEX_PAGE_ID || ''
+export const SUBSCRIPTION_PAGE_ID = import.meta.env.SUBSCRIPTION_PAGE_ID || process.env.SUBSCRIPTION_PAGE_ID || ''
 
-export const CUSTOM_DOMAIN = process.env.CUSTOM_DOMAIN || 'alpacat.com' // <- Set your costom domain if you have. e.g. alpacat.com
-export const BASE_PATH = process.env.BASE_PATH || '' // <- Set sub directory path if you want. e.g. /docs/
+export const CUSTOM_DOMAIN = import.meta.env.CUSTOM_DOMAIN || process.env.CUSTOM_DOMAIN || 'alpacat.com' // <- Set your costom domain if you have. e.g. alpacat.com
+export const BASE_PATH = import.meta.env.BASE_PATH || process.env.BASE_PATH || '' // <- Set sub directory path if you want. e.g. /docs/
 
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID
 export const NUMBER_OF_POSTS_PER_PAGE = 10
